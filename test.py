8fba53fbf4a59cd1c57c6d2427422a56eed9bb44
Odd Hole <oddh@axis.com>
2019-06-11 10:22:47 +0200

    merge conflict resolved
    Change-Id: I2ad00aa4e1b8c5edbf3c967554898ff1bab347b4

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def main(msg):
-<<<<<<< HEAD
     # Adding a different comment
-=======
     # prints a message
->>>>>>> refs/remotes/origin/master
     print(msg)
 
 main("Hello all!")
